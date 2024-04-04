48c7375455a82f3581f9e4ae6242977ba2ffcb9b
Vadim Sigutin <vadim.sig09@gmail.com>
2024-04-04 07:55:25 +0600

    fix: images sizes

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -25,7 +25,6 @@ const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
 				src={product.thumbnail}
 				width='300'
 				height='150'
-				style={{ width: 'auto', height: 'auto' }}
 				className='h-[150px] w-[300px] object-fill'
 			/>
 			<div className='mt-5 px-5 pb-5'>
